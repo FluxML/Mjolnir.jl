1478b75f4a2108dc3024870bf8278a7d4e3ceb20
Mike J Innes <mike.j.innes@gmail.com>
2019-09-27 11:55:28 +0100

    rename flow.jl

--- src/abstract/Abstract.jl
+++ src/abstract/Abstract.jl
@@ -5,7 +5,7 @@ using IRTools: IR, CFG, Variable, block, blocks, arguments, argtypes, isexpr,
   stmt, branches, isreturn, returnvalue, argument!, return!
 
 include("utils.jl")
-include("interpreter.jl")
+include("flow.jl")
 include("trace.jl")
 include("base.jl")
 
