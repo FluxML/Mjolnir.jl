c01961e1ba4d7a6e196508b423f0dcf77db20d83
Mike Innes <mike.j.innes@gmail.com>
2019-10-15 13:46:09 +0100

    observe changes results in general

--- src/inference/trivial.jl
+++ src/inference/trivial.jl
@@ -7,6 +7,7 @@ struct Trivial end
 function infer(f, ::Trivial, tr = trace(typeof(f)))
   r = returntype(tr)
   r isa Abstract.Const && return Singleton(r.value)
+  any(((v, st),) -> iscall(st.expr, observe), tr) && return
   r = returnvalue(blocks(tr)[end])
   r isa Variable || return
   ex = tr[r].expr
--- test/inference.jl
+++ test/inference.jl
@@ -9,6 +9,12 @@ Random.seed!(0)
 @test infer(() -> rand(Normal(0, 1))) == Normal(0, 1)
 @test infer(() -> rand(Bernoulli(0.5))) == Bernoulli(0.5)
 
+@test infer() do
+  x = rand(Normal(0, 1))
+  observe(x > 0)
+  x
+end isa Poirot.Empirical
+
 d = infer() do
   i = rand(Uniform(1,10))
   i^2 > 50
