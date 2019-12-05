5bdd7ce381c42e0f194891c5a6a76a74ab214c1f
Mike Innes <mike.j.innes@gmail.com>
2019-12-05 16:02:37 +0000

    more general distributions support

--- src/abstract/lib/random.jl
+++ src/abstract/lib/random.jl
@@ -2,7 +2,7 @@ using Distributions
 
 abstract(::AType{typeof(rand)}) = Float64
 abstract(::AType{typeof(randn)}) = Float64
-abstract(::AType{typeof(rand)}, ::AType{Uniform{T}}) where T = T
-abstract(::AType{typeof(rand)}, ::AType{Normal{T}}) where T = T
-abstract(::AType{typeof(rand)}, ::AType{<:Bernoulli}) where T = Bool
 abstract(::AType{typeof(rand)}, ::AType{<:Type{Bool}}) where T = Bool
+
+abstract(::AType{typeof(rand)}, T::AType{<:Distribution}) =
+  Core.Compiler.return_type(rand, Tuple{widen(T)})
