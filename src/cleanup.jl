1de048af5ce9c1dbd6f90df9371c310b63b491ee
Mike J Innes <mike.j.innes@gmail.com>
2020-04-09 11:43:05 +0100

    correct dce for plain variables

--- src/cleanup.jl
+++ src/cleanup.jl
@@ -33,6 +33,8 @@ function deadcode!(ir::IR)
       if isexpr(ir[v].expr)
         effectful(exprtype.((ir,), ir[v].expr.args)...) && continue
         map(v -> v isa Variable && (us[v] -= 1), ir[v].expr.args)
+      else ir[v].expr isa Variable
+        us[ir[v].expr] -= 1
       end
       delete!(ir, v)
     end
