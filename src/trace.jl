d67d4a181421255265d042d2df33a75969c91c60
Mike J Innes <mike.j.innes@gmail.com>
2020-05-07 15:35:24 +0100

    trace docstrings

--- src/trace.jl
+++ src/trace.jl
@@ -215,6 +215,20 @@ function tracecall!(tr::Trace, args, Ts...)
   return result
 end
 
+"""
+    trace(P, Ts...)
+
+Trace the method signature `Ts` using the primtive set `P`. e.g.
+
+    julia> trace(Defaults(), typeof(+), Int, Int)
+    1: (%1 :: typeof(+), %2 :: Int64, %3 :: Int64)
+      %4 = (%1)(%2, %3) :: Int64
+      return %4
+
+    julia> trace(Defaults(), typeof(+), Const(2), Const(2))
+    1: (%1 :: typeof(+), %2 :: const(2), %3 :: const(2))
+      return 4
+"""
 function trace(P, Ts...)
   tr = Trace(P)
   try
@@ -243,6 +257,27 @@ function tracem(P, ex)
   :(trace($(esc(P)), atype.(($(esc(f)), $(esc.(args)...)))...))
 end
 
+"""
+    @trace f(args...)
+    @trace P f(args...)
+
+Get a typed trace for `f`, analagous to `@code_typed`. Note that unlike
+`@code_typed`, you probably want to pass types rather than values, e.g.
+
+    julia> @trace Int+Int
+    1: (%1 :: const(+), %2 :: Int64, %3 :: Int64)
+      %4 = (+)(%2, %3) :: Int64
+      return %4
+
+If you instead pass actual integers, Mjolnir will aggressively
+constant-propagate them, resulting in a trivial trace.
+
+    julia> @trace 2+2
+    1: (%1 :: const(+), %2 :: const(2), %3 :: const(2))
+      return 4
+
+`P` is the primitive set used, which is `Mjolnir.Defaults()` by default.
+"""
 macro trace(P, ex)
   tracem(P, ex)
 end
