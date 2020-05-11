09e65ccd299f77a82fd6e4e81e3c91c8a2d5ad94
Shashi Gowda <shashigowda91@gmail.com>
2020-05-10 23:34:02 -0400

    add some pure numeric functions

--- src/lib/numeric.jl
+++ src/lib/numeric.jl
@@ -1,6 +1,13 @@
 struct Numeric end
 
-@pure Numeric +, -, *, /, &, |, ^, !, >, >=, <, <=, ==, !=, sin, cos, tan, float
+# scalar ops
+
+@pure Numeric !, !=, &, *, +, -, /, <, <=, ==, >, >=, ^, |, abs, abs2, acos, acosd,
+              acosh, acot, acotd, acoth, acsc, acscd, acsch, asec, asecd, asech,
+              asin, asind, asinh, atan, atand, atanh, cbrt, conj, cos, cosd, cosh,
+              cospi, cot, cotd, coth, csc, cscd, csch, deg2rad, exp, exp10, exp2,
+              expm1, float, inv, log, log10, log1p, log2, rad2deg, sec, secd, sech,
+              sin, sind, sinh, sinpi, sqrt, tan, tand, tanh, transpose
 
 @abstract Numeric rand() = Float64
 @abstract Numeric randn() = Float64
